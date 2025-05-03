2c6ea59e80254af6b343c147e12b720e9b2932d0
liusiyuan <3104959526@qq.com>
2025-05-03 11:54:05 +0800

    improve test file

--- test/HNSW_Persistent_Test_Phase1.cpp
+++ test/HNSW_Persistent_Test_Phase1.cpp
@@ -3,27 +3,41 @@
 #include <vector>
 
 std::vector<std::string> load_text(std::string filename) {
-  std::ifstream file(filename);
-  std::string line;
-  std::vector<std::string> text;
-  while (std::getline(file, line)) {
-    text.push_back(line);
-  }
-  return text;
+    std::ifstream file(filename);
+    std::string line;
+    std::vector<std::string> text;
+    while (std::getline(file, line)) {
+        text.push_back(line);
+    }
+    return text;
 }
 
 int main() {
-  KVStore store("data/");
-  store.reset();
+    KVStore store("data/");
+    store.reset();
 
-  std::vector<std::string> text = load_text("data/trimmed_text.txt");
-  int total = 128;
-  for (int i = 0; i < total; i++) {
-    store.put(i, text[i]);
-  }
+    std::vector<std::string> text = load_text("data/trimmed_text.txt");
+    const int total = 128;
+    const int phase[4] = {0, 32, 64, 96};
 
-  // TODO: uncomment this line when you have implemented the function
-  store.save_hnsw_index_to_disk("hnsw_data/");
+    for (int i = 0; i < total; i++) {
+        store.put(i, text[i]);
+    }
 
-  return 0;
+    for(int i = phase[1]; i < total; ++i) {
+        store.del(i);
+    }
+
+    for(int i = phase[2]; i < phase[3]; ++i) {
+        store.put(i, text[i]);
+    }
+
+    for(int i = phase[0]; i < phase[1]; ++i) {
+        int j = i + phase[3];
+        store.put(i, text[j]);
+    }
+
+    store.save_hnsw_index_to_disk("hnsw_data/");
+
+    return 0;
 }
